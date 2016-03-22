8260364786d6c439995425683e8aa04b62821882
Bruno Passos <bruno.passos@booking.com>
2016-03-22 09:37:00 +0100

    preparing for users login and log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   get 'users/new'
-
+  
   root             'static_pages#home'
-
-  get 'help'    => 'static_pages#help'
-
-  get 'about'   => 'static_pages#about'
-
-  get 'contact' => 'static_pages#contact'
-
-  get 'signup'  => 'users#new'
+  get     'help'    => 'static_pages#help'
+  get     'about'   => 'static_pages#about'
+  get     'contact' => 'static_pages#contact'
+  get     'signup'  => 'users#new'
+  get     'login'   => 'sessions#new'
+  post    'login'   => 'sessions#create'
+  delete  'logout'  => 'sessions#destroy'
   
   resources :users
 
