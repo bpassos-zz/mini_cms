8b33282070592bdbe7b085e5a8b4ff5335b90983
Bruno Passos <bruno.passos@booking.com>
2016-04-07 10:12:13 +0200

    Changing things to Mini CMS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	# Returns the full title on a per-page basis.
 	def full_title(page_title = '')
-		base_title = "Ruby on Rails tutorial Sample App"
+		base_title = "Mini CMS"
 		if page_title.empty?
 			base_title
 		else
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "Ruby on Rails Tutorial Mini CMS"
   end
 
   test "should get home" do
