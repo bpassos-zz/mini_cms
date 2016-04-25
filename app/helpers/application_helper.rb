44244f7871894087e6ce9395dcd3aebfbe6eb6dc
Bruno Passos <bruno.passos@booking.com>
2016-04-25 19:38:41 +0200

    Edit functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,21 @@
 module ApplicationHelper
 
-	# Returns the full title on a per-page basis.
-	def full_title(page_title = '')
-		base_title = "Mini CMS"
-		if page_title.empty?
-			base_title
-		else
-			page_title + " | " + base_title
-		end
-	end
+    # Returns the full title on a per-page basis.
+    def full_title(page_title = '')
+      base_title = "Mini CMS"
+      if page_title.empty?
+        base_title
+      else
+        page_title + " | " + base_title
+      end
+    end
+
+    # Returns user name spliter
+    def first_name(user)
+      user.name.split(" ")[0..-2].join(" ")
+    end
+
+    def last_name(user)
+      user.name.split(" ").last
+    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,5 @@ module UsersHelper
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
+  
 end
